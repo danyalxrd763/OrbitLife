b162636c6f583510fe1042eaeb607ac506992d7a
danyalxrd763 <ryabovartem30@gmail.com>
2026-09-20 13:20:46 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,13 @@
 import os
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+if not BOT_TOKEN:
+    raise RuntimeError(
+        "Не найден BOT_TOKEN. Добавь его в переменные окружения или файл .env"
+    )
