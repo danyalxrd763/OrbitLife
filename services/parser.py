59642b5f4f183ef3d567cc8246136ac1c873c872
danyalxrd763 <ryabovartem30@gmail.com>
2026-09-22 21:42:04 +0400

    Create parser.py

---
+++ services/parser.py
@@ -0,0 +1,42 @@
+import re
+from services.intents import Intent
+
+expense_words = [
+    "отдал","потратил","купил","заплатил"
+]
+
+income_words = [
+    "получил","зарплата","пришло"
+]
+
+def detect_intent(text: str):
+    text = text.lower()
+
+    amount = None
+    found = re.findall(r"\d+", text)
+
+    if found:
+        amount = int(found[0])
+
+    if "напомни" in text:
+        return Intent.REMINDER, {"text": text}
+
+    if "запиши" in text:
+        return Intent.NOTE, {"text": text}
+
+    if any(w in text for w in expense_words):
+        return Intent.EXPENSE, {
+            "amount": amount,
+            "text": text
+        }
+
+    if any(w in text for w in income_words):
+        return Intent.INCOME, {
+            "amount": amount,
+            "text": text
+        }
+
+    if "сколько" in text or "статистика" in text:
+        return Intent.ANALYTICS, {}
+
+    return Intent.CHAT, {"text": text}
