075072c71cc64c877ede2ca17a00dc90f25558fd
danyalxrd763 <ryabovartem30@gmail.com>
2026-09-22 21:33:58 +0400

    Delete services directory

--- services/intents.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class Intent(str, Enum):
-    EXPENSE = "expense"
-    INCOME = "income"
-    REMINDER = "reminder"
-    NOTE = "note"
-    ANALYTICS = "analytics"
-    CHAT = "chat"
